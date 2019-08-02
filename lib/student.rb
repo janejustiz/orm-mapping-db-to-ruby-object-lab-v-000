d96ddfd3c4925938a912ae681aaeeeb5deed177a
Jane Justiz <janejustiz@gmail.com>
2019-08-02 00:47:13 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -58,4 +58,19 @@ class Student
     sql = "DROP TABLE IF EXISTS students"
     DB[:conn].execute(sql)
   end
+
+  def self.find_by_name
+  end
+
+  def self.students_below_12th_grade
+  end
+
+  def self.first_X_students_in_grade_10
+  end
+
+  def self.first_student_in_grade_10
+  end
+
+  def self.all_students_in_grade_X
+  end
 end
