d2104ba7e0a8a5bef13b65476ca591f5c5e7bdbe
Jane Justiz <janejustiz@gmail.com>
2019-08-02 00:52:19 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -70,14 +70,39 @@ class Student
   end
 
   def self.students_below_12th_grade
+    sql = <<-SQL
+    SELECT *
+    FROM students
+    WHERE grade != "12"
+    SQL
+
+    DB[:conn].execute(sql)
   end
 
   def self.first_X_students_in_grade_10
+    sql = <<-SQL
+    SELECT *
+    FROM students
+    SQL
+
+    DB[:conn].execute(sql)
   end
 
   def self.first_student_in_grade_10
+    sql = <<-SQL
+    SELECT *
+    FROM students
+    SQL
+
+    DB[:conn].execute(sql)
   end
 
   def self.all_students_in_grade_X
+    sql = <<-SQL
+    SELECT *
+    FROM students
+    SQL
+
+    DB[:conn].execute(sql)
   end
 end
