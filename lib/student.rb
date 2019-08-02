48155d744ef761010c9e6045ee416c4224c638ce
Jane Justiz <janejustiz@gmail.com>
2019-08-02 00:56:02 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -71,12 +71,14 @@ class Student
 
   def self.students_below_12th_grade
     sql = <<-SQL
-    SELECT name
+    SELECT *
     FROM students
     WHERE grade != "12"
     SQL
 
-    DB[:conn].execute(sql)
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
   end
 
   def self.first_X_students_in_grade_10
